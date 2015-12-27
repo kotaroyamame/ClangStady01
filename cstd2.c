b4cb02b51c7213f25c6537331c90ced443273c7a
kotaro <kotaro@kotaro-no-MacBook-Air.local>
2015-12-27 05:09:02 -0500

    point stay

---
+++ cstd2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *q,int *p);
+
+void swap(int *q,int *p){
+    int w;
+    w=*q;
+    *q=*p;
+    *p=w;
+    return;
+}
+int main(void) {
+    int x=0,y=1;
+    swap(&x,&y);
+    printf("x:%d y:%d \n",x,y);
+	return 0;
+}
