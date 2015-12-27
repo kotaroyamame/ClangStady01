b36c3b9570ef899fa12fe44a70795eebd5d1f502
kotaroyamame <yamame@fi.chu.jp>
2015-12-27 16:41:42 -0500

    search

--- cstd3.c
+++ cstd3.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 struct human{
     struct human *next;
     char name[20];
     int eage;
 };
+void show_human(struct human *head){
+    struct human *p;
+    for(p = head;p != NULL;p = p->next){
+        printf("name:%s eage:%d \n",p->name,p->eage);
+        
+    }
+    return;
+}
 int main(void) {
     struct human humans[] = {
         {&humans[1],"ueda",18},
@@ -14,12 +23,16 @@ int main(void) {
 //    struct human ueda={NULL,"ueda",18};
 //    struct human iida={&ueda,"iida",25};
 //    struct human aizawa={&iida,"aizawa",20};
-    
-    struct human *head=&humans[0];
+    struct human *head=humans;
     struct human *p;
-    for(p = head;p != NULL;p = p->next){
+    show_human(head);
+    char name[20];
+    printf("name>");
+    scanf("%s",name);
+          
+    for(p=head;p!=NULL;p=p->next){
+        if(strcmp(name,p->name)==0)
         printf("name:%s eage:%d \n",p->name,p->eage);
-        
     }
     return 0;
     
