5c73c5b5ad7e66a69111284e578edb318343e0fc
kotaroyamame <yamame@fi.chu.jp>
2015-12-27 16:08:31 -0500

    struct array

--- cstd3.c
+++ cstd3.c
@@ -6,11 +6,16 @@ struct human{
     int eage;
 };
 int main(void) {
-    struct human ueda={NULL,"ueda",18};
-    struct human iida={&ueda,"iida",25};
-    struct human aizawa={&iida,"aizawa",20};
+    struct human humans[] = {
+        {&humans[1],"ueda",18},
+        {&humans[2],"iida",25},
+        {NULL,"aizawa",20},
+    };
+//    struct human ueda={NULL,"ueda",18};
+//    struct human iida={&ueda,"iida",25};
+//    struct human aizawa={&iida,"aizawa",20};
     
-    struct human *head=&aizawa;
+    struct human *head=&humans[0];
     struct human *p;
     for(p = head;p != NULL;p = p->next){
         printf("name:%s eage:%d \n",p->name,p->eage);
